202412ec91e9c42f3ab05a582f3400788e448f79
Elad Kehat <eladkehat@gmail.com>
2019-03-25 21:31:50 +0200

    Upgraded lathorp to 0.2

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pathlib
 import pytest
 
 # Import the fixtures to make them available to the tests
-from lathorp.fixtures import load_schema_definitions
+from lathorp import load_schema_definitions
 from lathorp.fixtures import pg  # noqa: F401
 from lathorp.fixtures import pg_connect  # noqa: F401
 
@@ -13,7 +13,7 @@ def init_schema(pg):  # noqa: F811
     """Loads the schema definition."""
     path = pathlib.Path(__file__).parent / 'pg_ddl'
     assert path.is_dir()
-    load_schema_definitions(pg, path)
+    load_schema_definitions(pg.dsn(), path)
 
 
 @pytest.fixture(scope='function')
